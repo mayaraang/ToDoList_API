9aa3d4fc3796b28493b2d2ff1bd1facfd215b884
Mayara Angélica <mayaraangelica411@gmail.com>
2025-04-01 16:35:27 -0300

    fix: fix database config error

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,5 @@
 const sqlite3 = require('sqlite3').verbose()
-const db = new sqlite3.Database('./db/todo.db')
+const db = new sqlite3.Database('./db/ToDoList.db')
 
 db.serialize(() => {
   db.run("CREATE TABLE IF NOT EXISTS tasks (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, description TEXT, completed BOOLEAN)")
