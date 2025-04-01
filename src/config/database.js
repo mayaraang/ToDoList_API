eadb1fccf4f744ad193adcff5277a07b2329ca75
Mayara Angélica <mayaraangelica411@gmail.com>
2025-04-01 16:04:06 -0300

    feat: add database config

---
+++ src/config/database.js
@@ -0,0 +1,8 @@
+const sqlite3 = require('sqlite3').verbose()
+const db = new sqlite3.Database('./db/todo.db')
+
+db.serialize(() => {
+  db.run("CREATE TABLE IF NOT EXISTS tasks (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, description TEXT, completed BOOLEAN)")
+})
+
+module.exports = db
